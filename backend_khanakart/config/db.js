8b7f6ef7991b48a2da108572e553e62bb16fd201
pankajpk115 <pankajpkpk909@gmail.com>
2024-09-25 17:24:56 +0530

    Latest

--- backend_khanakart/config/db.js
+++ backend_khanakart/config/db.js
@@ -1,2 +1,7 @@
+import mongoose from "mongoose";
 
+export const connectDB = async ()=>{
+    mongoose.connect('mongodb+srv://pankajpkpk909:pankajpk115@cluster0.rayyz.mongodb.net/khanakart').then(()=>console.log("DB connected!")
+    );
+}
 
